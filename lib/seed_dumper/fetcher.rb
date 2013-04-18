1bfe556ac4f6e45db6eebd35fa05e69715048f9b
Nathan Hyde <nhyde@mbayaq.org>
2013-04-18 15:16:50 -0700

    Fix issue where a datetime column value wasn't being enclosed in quotes (in fetcher.rb)

--- lib/seed_dumper/fetcher.rb
+++ lib/seed_dumper/fetcher.rb
@@ -2,19 +2,19 @@ module SeedDumper
 
   # Dumper
   class Fetcher
-    
+
     def self.fetch_data(klass, options={})
       ignore = ['created_at', 'updated_at']
       ignore += options[:ignore].map(&:to_s) if options[:ignore].is_a? Array
       model_name = klass.name
-      
+
       puts "Adding #{model_name.camelize} seeds."
-      
-      records = klass.all.map do |record| 
+
+      records = klass.all.map do |record|
         attr_s = [];
-      
+
         record.attributes.delete_if { |k, v| ignore.include?(k) }.each do |key, value|
-          value = value.class == Time ? "\"#{value}\"" : value.inspect
+          value = [Time, ActiveSupport::TimeWithZone].include?(value.class) ? "\"#{value}\"" : value.inspect
           value = nil if value.is_a?(String) && value == "\"\""
           value = nil if value == 'nil' || value == "nil"
 
@@ -22,16 +22,16 @@ module SeedDumper
             attr_s.push("#{key.to_sym.inspect} => #{value}")# unless key == 'id'
           end
         end
-      
+
         record_dump = "#{model_name.camelize}.create(#{attr_s.join(', ')})"
         record_dump = "#{record_dump}{|record| record.id = #{record.attributes['id']}}" if options[:dump_id] && record.attributes['id']
         record_dump
       end
       # / records.each_with_index
-      
+
       records
     end
 
   end
-  
+
 end
